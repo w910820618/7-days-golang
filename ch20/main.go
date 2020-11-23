92462a7f36f25b5823311abe6cdec906fa25e526
吴洲洋 <w910820618@163.com>
2020-11-23 21:09:03 +0800

    how get the token in header

--- ch20/main.go
+++ ch20/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func HandlerToken(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	fmt.Println(r.Header.Get("AuthToken"))
+}
+
+func main() {
+	http.HandleFunc("/token", HandlerToken)
+
+	http.ListenAndServe(":8080", nil)
 }
